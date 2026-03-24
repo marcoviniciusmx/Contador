ee22025bf67719cb7e8b4acea01b5a02d929c010
marcoviniciusmx <marco10091010@gmail.com>
2026-03-24 13:14:57 -0300

    refactor: reorganize structure by creating pages, components and Home folders, and adjust Home module

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GlobalStyles } from './styles/GlobalStyles'
-import Home from './App.jsx'
+import Home from './pages/Home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
